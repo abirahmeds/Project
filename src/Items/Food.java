e72b850338a8d3224be95165e624234c15719d58
abirahmeds <43213194+abirahmeds@users.noreply.github.com>
2018-11-09 05:06:49 -0500

    Update and rename Food to Food.java

--- src/Items/Food.java
+++ src/Items/Food.java
@@ -1,13 +1,15 @@
 //Abir Ahmed
 package Items;
 
+import People.Person;
+
 /**
  * Interface for all food items.
  */
 public interface Food {
     /**
      * Abstract method that will behave differently based on the item.
-     * ~Overridden in Mushroom
+     * ~Overridden in Mushroom, Grapes
      * @param player Current Person.
      */
     public void eat(Person player);
