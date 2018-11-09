2415421e5888376b78d76f04fc87c7ff286391e3
abirahmeds <43213194+abirahmeds@users.noreply.github.com>
2018-11-09 03:35:20 -0500

    Create Mushroom.java

---
+++ src/Items/Mushroom.java
@@ -0,0 +1,44 @@
+//Abir Ahmed
+package Items;
+
+/**
+ * Mushroom Entity; can be eaten or collected.
+ */
+public class Mushroom extends Entity  implements Consumable{
+    private boolean poisonous;
+
+    /**
+     * Main Constructor
+     * @param x position of the Mushroom along the x Axis. (Row)
+     * @param y position of the Mushroom along the y Axis. (Column)
+     * @param poisonous The boolean value representing whether an instance of a Mushroom is poisonous.
+     */
+    public Mushroom(int x,  int y, boolean poisonous) {
+        super(x, y);
+        this.poisonous = poisonous;
+    }
+
+    @Override
+    /**
+     * Changes Person attributes (fields) based on whether Mushroom is poisonous.
+     * Not all of these changes are positive.
+     */
+    public void eat (Person p) {
+        if (!poisonous) {
+            p.restoreHealth(1);
+            p.restoreStamina(1);
+            p.restoreNutrition(2);
+            p.restoreHydration(2);
+        } else {
+            p.restoreHealth(0);
+            p.restoreStamina(0);
+            p.restoreNutrition(0);
+            p.restoreHydration(0);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return ("Mushroom");
+    }
+}
