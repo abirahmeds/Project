251ee7162c887d55e7009f69b512fea6dc2de638
abirahmeds <43213194+abirahmeds@users.noreply.github.com>
2018-11-09 05:15:42 -0500

    Update and rename Potion to Potion.java

--- src/Items/Potion.java
+++ src/Items/Potion.java
@@ -1,13 +1,15 @@
 //Abir Ahmed
 package Items;
 
+import People.Person;
+
 /**
  * Interface for all potion items.
  */
 public interface Potion {
     /**
      * Abstract method that will behave differently based on the item.
-     *
+     *Overridden in EndurancePotion, StrengthPotion, HealthPotion, IntellectPotion
      * @param player Current Person.
      */
     public void drink(Person player);
