4b4bcbc40e72e8444248533e8e5d05f0c7090f69
abirahmeds <43213194+abirahmeds@users.noreply.github.com>
2018-11-09 05:03:53 -0500

    Update and rename StaminaPotion to StaminaPotion.java

---
+++ src/Items/StaminaPotion.java
@@ -0,0 +1,30 @@
+//Abir Ahmed
+package Items;
+
+/**
+ * Endurance Potion; can be consumed or collected.
+ */
+public class EndurancePotion extends Item  implements Potion{
+    
+    /**
+     * Main Constructor
+     * @param x position of the Potion along the x Axis. (Row)
+     * @param y position of the Potion along the y Axis. (Column)
+     */
+    public EndurancePotion(int x,  int y) {
+        super(x, y);
+    }
+
+    @Override
+    /**
+     * Restores Peron's Endurance.
+     */
+    public void drink (Person p) {
+        p.setEndurance(100);
+    }
+
+    @Override
+    public String toString() {
+        return ("Endurance Potion");
+    }
+}
